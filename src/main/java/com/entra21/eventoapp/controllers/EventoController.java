ea5d78a0da05ad2a9e70a2c389cdd8e71fb52dd0
SFStarke <sergiostarke@gmail.com>
2022-09-06 11:42:50 -0300

    Entra21 01 06/09/22

--- src/main/java/com/entra21/eventoapp/controllers/EventoController.java
+++ src/main/java/com/entra21/eventoapp/controllers/EventoController.java
@@ -1,14 +1,20 @@
 package com.entra21.eventoapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.entra21.eventoapp.models.Convidado;
 import com.entra21.eventoapp.models.Evento;
+import com.entra21.eventoapp.repository.ConvidadoRepository;
 import com.entra21.eventoapp.repository.EventoRepository;
 
 @Controller
@@ -16,6 +22,8 @@ public class EventoController {
 	
 	@Autowired
 	private EventoRepository er;
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	@RequestMapping(value = "/cadastrarEvento", method= RequestMethod.GET)
 	public String form(){
@@ -36,11 +44,37 @@ public class EventoController {
 						   //que está no HTML		
 		return mv;
 	}
-	@RequestMapping("/{codigo}")
+	
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
-		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento"); 
 		mv.addObject("evento", evento);
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
 		return mv;
 	}
+	
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo,
+			@Valid Convidado convidado, BindingResult result,
+			RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+	        attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+	        return "redirect:/{codigo}";
+	    }
+	    Evento evento = er.findByCodigo(codigo);
+	    convidado.setEvento(evento);
+	    cr.save(convidado);
+	    attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
+	    return "redirect:/{codigo}";
+	    
+//		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+//		Evento evento = er.findByCodigo(codigo);
+//		convidado.setEvento(evento);
+//		cr.save(convidado);
+//		mv.addObject("evento", evento);
+//		return "redirect:/{codigo}";
+	}
+	
 }
