08387c0cbc1cbd3266874edc5c13ae2196ef5333
SFStarke <sergiostarke@gmail.com>
2022-09-13 11:58:19 -0300

    Entra21 01 13/09/22

--- src/main/java/com/entra21/eventoapp/controllers/IndexController.java
+++ src/main/java/com/entra21/eventoapp/controllers/IndexController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	
 	@RequestMapping("/")
-	public String index() {
-		return "index";
+	public String index() {	
+		return "index";	
+	}
+	@RequestMapping("/login")
+	public String login() {	
+		return "login";	
 	}
 }
--- src/main/java/com/entra21/eventoapp/security/WebSecurityConfig.java
+++ src/main/java/com/entra21/eventoapp/security/WebSecurityConfig.java
@@ -1,8 +1,37 @@
 package com.entra21.eventoapp.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration //Classe de Configuração
+@EnableWebSecurity // Habilita Sprig Security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().loginPage("/login").permitAll() 
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		 auth.inMemoryAuthentication() // autenticação em memoria
+		.withUser("hello").password("{noop}123").roles("ADMIN");
+	}
+	
+
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/materialize/**", "/style/**");
+	}
 
 	
 }
