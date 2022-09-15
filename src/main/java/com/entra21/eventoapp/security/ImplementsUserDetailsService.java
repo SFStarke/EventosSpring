0ce404ca9fe2ba7e3ec905fb3c50029cb042d08a
SFStarke <sergiostarke@gmail.com>
2022-09-15 11:58:36 -0300

    Entra21 01 15/09/22

---
+++ src/main/java/com/entra21/eventoapp/security/ImplementsUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.entra21.eventoapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import com.entra21.eventoapp.models.Usuario;
+import com.entra21.eventoapp.repository.UsuarioRepository;
+
+@Repository //Esta anotation é requerida por capturar exeções de persistência
+public class ImplementsUserDetailsService  implements UserDetailsService{
+	@Autowired
+	private UsuarioRepository ur; // injeção de dependência
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		Usuario usuario = ur.findByLogin(username); //Busca o usuario pelo nome
+		
+		if(usuario == null) {
+		throw new UsernameNotFoundException("Usuário não Encontrado");
+		}
+		return usuario;
+	}
+	
+}
+
--- src/main/java/com/entra21/eventoapp/security/WebSecurityConfig.java
+++ src/main/java/com/entra21/eventoapp/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.entra21.eventoapp.security;
 
+import org.aspectj.weaver.ast.And;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,11 +9,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration //Classe de Configuração
 @EnableWebSecurity // Habilita Sprig Security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ImplementsUserDetailsService userDetailsService; // injeção de dependência
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable().authorizeRequests()
@@ -23,8 +30,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		 auth.inMemoryAuthentication() // autenticação em memoria
-		.withUser("hello").password("{noop}123").roles("ADMIN");
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+		
+//		 auth.inMemoryAuthentication() // autenticação em memoria // TENQUE SER CORRIGIDA
+//		.withUser("hello").password("{noop}123").roles("ADMIN")
+//		.and().withUser("hello").password("{noop}123").roles("RESTRICT");
 	}
 	
 
